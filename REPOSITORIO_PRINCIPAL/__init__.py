40152549640381ae98fbc326188ac4b0cb42a71e
UrielFrontDeveloper <urielzft@gmail.com>
2025-03-12 21:35:17 -0500

    New Carpets

---
+++ REPOSITORIO_PRINCIPAL/__init__.py
@@ -0,0 +1,2 @@
+def saludar (nomrbe):
+    return "hola " + nombre + " Este es mi primer paquete pip!"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repositorio principal",           # Nombre del paquete
+    version="0.1.0",                # Versión
+    packages=find_packages(),       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="IHKA",         # Tu nombre
+    author_email="[urielzft@gmail.com]",  # Tu correo electrónico
+    url="https://github.com/urielFrontendDeveloper/REPOSITORIO_PRINCIPAL",     # URL del proyecto
+)
